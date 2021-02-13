56e50c08e90ccdf03ee034dc103a60bfde7c79e1
devnagoyal-ux <58598904+devnagoyal-ux@users.noreply.github.com>
2021-02-13 14:13:13 +0530

    Update MyHeader.js

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,13 +1,19 @@
 import React, { Component} from 'react';
 import { Header,Icon,Badge } from 'react-native-elements';
 import { View, Text, StyeSheet ,Alert} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const MyHeader = props => {
   return (
+    <SafeAreaProvider>
+      <View style = {{marginBottom:1}}>
     <Header
+      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
       centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
       backgroundColor = "#eaf8fe"
     />
+    </View>
+    </SafeAreaProvider>
   );
 };
 
