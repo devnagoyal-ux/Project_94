d20c23463639fe019cee84306e207448deff2cd6
devnagoyal-ux <58598904+devnagoyal-ux@users.noreply.github.com>
2021-02-13 14:15:11 +0530

    Add files via upload

---
+++ components/CustomSideBarMenu.js
@@ -0,0 +1,26 @@
+import React, { Component} from 'react';
+import {StyleSheet, View, Text,TouchableOpacity} from 'react-native';
+import { DrawerItems} from 'react-navigation-drawer'
+
+import firebase from 'firebase';
+
+export default class CustomSideBarMenu extends Component{
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <View style={styles.drawerItemsContainer}>
+          <DrawerItems {...this.props}/>
+        </View>
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  container : {
+    flex:1
+  },
+  drawerItemsContainer:{
+    flex:0.8
+  }
+})
